08857d0c6dbea031560858a3da6fed2ae27f6e26
Jeffery Thomas <jeffery.thomas@gm.com>
2015-08-26 00:44:53 -0400

    Created new swift project for solution. Updated .gitignore for a swift project.

---
+++ VendingMachine/VendingMachine.h
@@ -0,0 +1,19 @@
+//
+//  VendingMachine.h
+//  VendingMachine
+//
+//  Created by Jeffery Thomas on 8/26/15.
+//  Copyright © 2015 JLT Source. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VendingMachine.
+FOUNDATION_EXPORT double VendingMachineVersionNumber;
+
+//! Project version string for VendingMachine.
+FOUNDATION_EXPORT const unsigned char VendingMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VendingMachine/PublicHeader.h>
+
+
